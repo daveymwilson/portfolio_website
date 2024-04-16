5b19f032b7992c1f7e07d1493acb785a4b86b438
daveymwilson <165616157+daveymwilson@users.noreply.github.com>
2024-04-15 23:00:17 -0700

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+console.log('Hello, World!');
+
+git remote add origin https://github.com/daveymwilson/Portfolio-Project.git
+git branch -M main
+git push -u origin main
+https://github.com/daveymwilson/Portfolio-Project.git
+
